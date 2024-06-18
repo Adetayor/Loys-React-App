c500127a43b365e2b747437002801e2b805bedc6
Samuel <adetayo456@gmail.com>
2024-06-18 07:35:49 +0100

    Rearranged

--- src/index.js
+++ src/index.js
@@ -16,6 +16,10 @@ import './index.css';
 // }
 
 // function Greeting() {
+//   return <h2>hello world</h2>;
+// }
+
+// function Greeting() {
 //   return (
 //     <div>
 //       <div>
@@ -85,10 +89,6 @@ import './index.css';
 // }
 
 // function Greeting() {
-//   return <h2>hello world</h2>;
-// }
-
-// function Greeting() {
 //   return (
 //     <div>
 //       <h2>john doe</h2>
@@ -114,47 +114,22 @@ import './index.css';
 //   return React.createElement('h2', {}, 'hello world');
 // }
 
-// const BookList = () =>{
-//   return <section>
-//     <Book/>
-//   </section>
-// }
-
 // const BookList = () => {
-//   return <section>
-//     <Book/>
-//     <Book/>
-//     <Book/>
-//     <Book/>
-//   </section>
+//     return <section> 
+//    </section>
+//  }
+
+// const Book =>{
+//   return <article>
+//
+//     </article>
+//    }
+
+// const Image = () => <h2>Image placeholder</h2>
+// const Title = () => <h2>Book</h2>
+// const Author = () => {
+// return <h3>Author</h3>;
 // }
-
-// const firstBook = {
-//   author: 'Michael Crichton & James Patterson',
-//   title: 'Eruption: Following Jurassic Park, Michael Crichton Started Another Masterpiece―James Patterson Just Finished It',
-//   img: './images/erupt.jpg',
-// };
-// const secondBook = {
-//   author: " Pete Hegseth",
-//   title: "The War on Warriors: Behind the Betrayal of the Men Who Keep Us Free",
-//   img: "https://m.media-amazon.com/images/I/7187im2CwUL._SY342_.jpg",
-// };
-
-const books = [
-  { author: 'Michael Crichton & James Patterson',
-    title: 'Eruption: Following Jurassic Park, Michael Crichton Started Another Masterpiece―James Patterson Just Finished It',
-    img: './images/erupt.jpg',
-},
-{
-  author: " Pete Hegseth",
-  title: "The War on Warriors: Behind the Betrayal of the Men Who Keep Us Free",
-  img: "https://m.media-amazon.com/images/I/7187im2CwUL._SY342_.jpg",
-}
-];
-
-// const author = 'Michael Crichton & James Patterson';
-// const title = 'Eruption: Following Jurassic Park, Michael Crichton Started Another Masterpiece―James Patterson Just Finished It';
-// const img= './images/erupt.jpg';
 
 // // parameters
 // const someFunc = (param1, param2) => {
@@ -164,75 +139,110 @@ const books = [
 // someFunc('job', 'developer');
 
 // const BookList = () => {
+//   return <section>
+//     <Book/>
+//   </section>
+// }
+
+// const BookList = () => {
+//   return <section>
+//     <Book/>
+//     <Book/>
+//     <Book/>
+//     <Book/>
+//   </section>
+// }
+
+//  const Book = () => {
 //   return (
-//     <section className='booklist'>
-//       <Book />
-//       <Book />
-//       <Book />
-//       <Book />
-//     </section>
+//    <article>
+//      <Image/>
+//      <Title/>
+//      <Author/>
+//    </article>
 //   );
+//  };
+
+// const Image = () => <h2>Image placeholder</h2>
+// const Title = () => <h2>Book</h2> or
+// const Title = () => <h2>Title of the Book</h2>
+// const Author = () => {
+// return <h3>Author</h3>;
+// }    or
+
+// const Image = () => (
+//   <img
+//      src="https://images-na.ssl-images-amazon.com/images/I/81SIQfF-aGL._AC_UL600_SR600,400_.jpg"
+//      alt="Eruption: Following Jurassic Park"
+//    />
+//  );
+// const Title = () => <h2>Eruption: Following Jurassic Park, Michael Crichton Started Another Masterpiece―James Patterson Just Finished It </h2>;
+// const Author = () => {
+//   return <h3>Michael Crichton & James Patterson</h3>;
+// };    or
+
+
+// const Image = () => (
+//   <img
+//     // src="https://images-na.ssl-images-amazon.com/images/I/81SIQfF-aGL._AC_UL600_SR600,400_.jpg"
+//     // src="./images/erupt.jpg"  alt="Eruption: Following Jurassic Park"
+//   />
+// );
+// // const Title = () => <h2>Book Title</h2>
+// const Title = () => 
+// {/* <h2>Eruption: Following Jurassic Park, Michael Crichton Started Another Masterpiece―James Patterson Just Finished It </h2> */
 // };
+// const Author = () => {
+//   const inlineHeadingStyles = { 
+//     color: '#617d98', 
+//     fontSize: '0.75rem', 
+//     marginTop: '0.5rem', 
+//   };
+//   // return <h3 style={{color:'#617d98',fontSize:'0.75rem', marginTop:'0.5rem'}}>
+//   //   by Michael Crichton & James Patterson</h3>;
+  
+// return 
+// // (<h3 style={inlineHeadingStyles}>
+// //       by Michael Crichton & James Patterson</h3>
+// //   );
+// };         or
+
+// const author = 'Michael Crichton & James Patterson';
+// const Book = () => {
+//   const title = 'Eruption: Following Jurassic Park, Michael Crichton Started Another Masterpiece―James Patterson Just Finished It';
+//   return (
+//     <article className="book">
+//       <img 
+//       src="./images/erupt.jpg" 
+//       alt="Eruption: Following Jurassic Park" 
+//       />
+//       <h2>
+//         {/* 'Eruption: Following Jurassic Park, Michael Crichton Started Another Masterpiece―James Patterson Just Finished It' */}
+//         {title}
+//       </h2>
+//       <h3>
+//         {/* by Michael Crichton & James Patterson */}
+//         {author}
+//         {/* {author.toUpperCase()} */}
+//       </h3>
+//       {/* {<p>{let x = 6}</p>}
+//       <p>{6 + 6}</p> */}
+//     </article>
+//   );
+// }; 
+
+// Props
+// const author = 'Michael Crichton & James Patterson';
+// const title = 'Eruption: Following Jurassic Park, Michael Crichton Started Another Masterpiece―James Patterson Just Finished It';
+// const img= './images/erupt.jpg';
 
 // const BookList = () => {
 //   return (
-//     <section className="booklist">
-//       <Book
-//         author={firstBook.author}
-//         title={firstBook.title}
-//         img={firstBook.img}
-//       ></Book>
-//       <Book
-//         author={secondBook.author}
-//         title={secondBook.title}
-//         img={secondBook.img}
-//       />
-//     </section>
-//   );
-// };
-
-const names = ['john', 'peter', 'susan'];
-const newNames = names.map((name) => {
-  return <h1>{name}</h1>;
-});
-console.log(newNames);
-const BookList = () => {
-  return <section className='booklist'>{names}</section>;
-};
-      const Book = (props) => {
-        const { img, title, author } = props;
-        console.log(props);
-        return (
-          <article className="book">
-            <img src={img} alt={title} />
-            <h2>{title}</h2>
-            <h4>{author}</h4>
-          </article>
-    );
-};
-
-// const BookList = () => {
-//   return (
-//     <section className="booklist">
-//       {/* <Book job="developer" />
-//       <Book title="random title" number={22} */}
-//       <Book
-//         author={firstBook.author}
-//         title={firstBook.title}
-//         img={firstBook.img}
-//       >
-//         <p>
-//           Lorem, Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-//           Itaque repudiandae inventore eos qui animi sed iusto alias eius ea
-//           sapiente.
-//         </p>
-//         <button>click me</button>
-//       </Book>
-//       <Book
-//         author={secondBook.author}
-//         title={secondBook.title}
-//         img={secondBook.img}
-//       />
+  //     <section className='booklist'>
+//       <Book />
+//       <Book />
+//       <Book />
+//       <Book />
 //     </section>
 //   );
 // };
@@ -257,17 +267,7 @@ const BookList = () => {
 //     </article>
 //   );
 // }; 
-
-// const Book = ({img, title, author, children}) => {
-//   return (
-//     <article className="book">  
-//       <img src={img} alt={title} />
-//       <h2>{title}</h2>
-//       <h3>{author}</h3>
-//       {children}
-//     </article>
-//   );
-// }; 
+ 
 
 // const Book = (props) => {
 //   console.log(props);
@@ -300,55 +300,125 @@ const BookList = () => {
 //   );
 // }; 
 
+//Dynamic Props Setup
+// const firstBook = {
+//   author: 'Michael Crichton & James Patterson',
+//   title: 'Eruption: Following Jurassic Park, Michael Crichton Started Another Masterpiece―James Patterson Just Finished It',
+//   img: './images/erupt.jpg',
+// };
+// const secondBook = {
+//   author: " Pete Hegseth",
+//   title: "The War on Warriors: Behind the Betrayal of the Men Who Keep Us Free",
+//   img: "https://m.media-amazon.com/images/I/7187im2CwUL._SY342_.jpg",
+// };
 
-// const author = 'Michael Crichton & James Patterson';
-// const Book = () => {
-//   const title = 'Eruption: Following Jurassic Park, Michael Crichton Started Another Masterpiece―James Patterson Just Finished It';
+// const BookList = () => {
 //   return (
-//     <article className="book">
-//       <img 
-//       src="./images/erupt.jpg" 
-//       alt="Eruption: Following Jurassic Park" 
+//     <section className="booklist">
+//       <Book
+//         author={firstBook.author}
+//         title={firstBook.title}
+//         img={firstBook.img}
+//       ></Book>
+//       <Book
+//         author={secondBook.author}
+//         title={secondBook.title}
+//         img={secondBook.img}
 //       />
-//       <h2>
-//         {/* 'Eruption: Following Jurassic Park, Michael Crichton Started Another Masterpiece―James Patterson Just Finished It' */}
-//         {title}
-//       </h2>
-//       <h3>
-//         {/* by Michael Crichton & James Patterson */}
-//         {author}
-//         {/* {author.toUpperCase()} */}
-//       </h3>
-//       {/* {<p>{let x = 6}</p>}
-//       <p>{6 + 6}</p> */}
+//     </section>
+//   );
+// };
+
+
+// const BookList = () => {
+//   return (
+//     <section className="booklist">
+//       {/* <Book job="developer" />
+//       <Book title="random title" number={22} */}
+//       <Book
+//         author={firstBook.author}
+//         title={firstBook.title}
+//         img={firstBook.img}
+//       >
+//         <p>
+//           Lorem, Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+//           Itaque repudiandae inventore eos qui animi sed iusto alias eius ea
+//           sapiente.
+//         </p>
+//         <button>click me</button>
+//       </Book>
+//       <Book
+//         author={secondBook.author}
+//         title={secondBook.title}
+//         img={secondBook.img}
+//       />
+//     </section>
+//   );
+// };
+// const Book = ({img, title, author, children}) => {
+//   return (
+//     <article className="book">  
+//       <img src={img} alt={title} />
+//       <h2>{title}</h2>
+//       <h3>{author}</h3>
+//       {children}
 //     </article>
 //   );
-// }; 
+// }; or
 
-// const Image = () => (
-//   <img
-//     // src="https://images-na.ssl-images-amazon.com/images/I/81SIQfF-aGL._AC_UL600_SR600,400_.jpg"
-//     // src="./images/erupt.jpg"  alt="Eruption: Following Jurassic Park"
-//   />
-// );
-// // const Title = () => <h2>Book Title</h2>
-// const Title = () => 
-// {/* <h2>Eruption: Following Jurassic Park, Michael Crichton Started Another Masterpiece―James Patterson Just Finished It </h2> */
+// const Book = (props) => {
+//   const { img, title, author } = props;
+//   console.log(props);
+//   return (
+//     <article className="book">
+//       <img src={img} alt={title} />
+//       <h2>{title}</h2>
+//       <h4>{author}</h4>
+//     </article>
+//   );
 // };
-// const Author = () => {
-//   const inlineHeadingStyles = { 
-//     color: '#617d98', 
-//     fontSize: '0.75rem', 
-//     marginTop: '0.5rem', 
-//   };
-//   // return <h3 style={{color:'#617d98',fontSize:'0.75rem', marginTop:'0.5rem'}}>
-//   //   by Michael Crichton & James Patterson</h3>;
-  
-// return 
-// // (<h3 style={inlineHeadingStyles}>
-// //       by Michael Crichton & James Patterson</h3>
-// //   );
-// };
+
+//Simple List
+const books = [
+  { author: 'Michael Crichton & James Patterson',
+    title: 'Eruption: Following Jurassic Park, Michael Crichton Started Another Masterpiece―James Patterson Just Finished It',
+    img: './images/erupt.jpg',
+},
+{
+  author: " Pete Hegseth",
+  title: "The War on Warriors: Behind the Betrayal of the Men Who Keep Us Free",
+  img: "https://m.media-amazon.com/images/I/7187im2CwUL._SY342_.jpg",
+}
+];
+
+
+const names = ['john', 'peter', 'susan'];
+const newNames = names.map((name) => {
+  return <h1>{name}</h1>;
+});
+console.log(newNames);
+const BookList = () => {
+  return (
+  <section className='booklist'>
+    {/* {newNames} */}
+    {names.map((name) => {
+    return <h1>{name}</h1>;
+  })}
+  </section>
+  );
+};
+      const Book = (props) => {
+        const { img, title, author } = props;
+        console.log(props);
+        return (
+          <article className="book">
+            <img src={img} alt={title} />
+            <h2>{title}</h2>
+            <h4>{author}</h4>
+          </article>
+    );
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 // root.render(<Greeting />);

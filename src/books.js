5be026013846a8d6a255f17ab4250169ee539e7f
Samuel <adetayo456@gmail.com>
2024-08-08 06:38:46 +0100

    Local Images (src folder)

--- src/books.js
+++ src/books.js
@@ -1,16 +1,29 @@
+import img1 from "./images/image1.jpg";
+import img2 from "./images/image2.jpg";
+import img3 from "./images/image3.jpg";
+
 export const books = [
   {
     author: "Michael Crichton & James Patterson",
     title:
       "Eruption: Following Jurassic Park, Michael Crichton Started Another Masterpiece―James Patterson Just Finished It",
-    img: "./images/erupt.jpg",
+    // img: "./images/erupt.jpg",
+    img: img1,
     id: 1,
   },
   {
     author: " Pete Hegseth",
     title:
       "The War on Warriors: Behind the Betrayal of the Men Who Keep Us Free",
-    img: "https://m.media-amazon.com/images/I/7187im2CwUL._SY342_.jpg",
+    // img: "https://m.media-amazon.com/images/I/7187im2CwUL._SY342_.jpg",
+    img: img2,
     id: 2,
+  },
+  {
+    author: "J. D. Vance",
+    title: "Hillbilly Elegy: A Memoir of a Family and Culture in Crisis",
+    // img: "https://images-na.ssl-images-amazon.com/images/I/81ytGlIkLoL._AC_UL600_SR600,400_.jpg",
+    img: img3,
+    id: 3,
   },
 ];

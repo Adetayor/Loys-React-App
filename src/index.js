86408411a66e6f36cc3e144c4d467cda74fd3aee
Samuel <adetayo456@gmail.com>
2024-08-08 07:28:13 +0100

    Numbers Challenge

--- src/index.js
+++ src/index.js
@@ -1015,8 +1015,8 @@ const BookList = () => {
   return (
     <section className="booklist">
       {/* <Book /> */}
-      {books.map((book) => {
-        return <Book {...book} key={book.id} />;
+      {books.map((book, index) => {
+        return <Book {...book} key={book.id} number={index} />;
       })}
     </section>
   );
